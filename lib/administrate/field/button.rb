ef5ec08bee84c45c1e06fd13a4f756536e27a66a
Jubilee <jubilee2@gmail.com>
2023-10-04 00:23:53 -0500

    add Rails::Engine

--- lib/administrate/field/button.rb
+++ lib/administrate/field/button.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 require 'administrate/field/base'
+require "rails"
 
 module Administrate
   module Field
     class Button < Administrate::Field::Base
-
+      class Engine < ::Rails::Engine; end
     end
   end
 end
