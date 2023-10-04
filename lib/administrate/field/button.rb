8497338fe94703a714a6c9af590771557ab3110e
Jubilee <jubilee2@gmail.com>
2023-10-03 22:38:10 -0500

    init commit

---
+++ lib/administrate/field/button.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "button/version"
+
+module Administrate
+  module Field
+    module Button
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
