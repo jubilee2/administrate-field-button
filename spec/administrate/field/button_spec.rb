d2201ffbf7efd3516fb46c977f442adce706cd70
Jubilee <jubilee2@gmail.com>
2023-10-07 00:18:27 -0500

    update rspec

--- spec/administrate/field/button_spec.rb
+++ spec/administrate/field/button_spec.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
 RSpec.describe Administrate::Field::Button do
-  it "has a version number" do
-    expect(Administrate::Field::Button::VERSION).not_to be nil
-  end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
